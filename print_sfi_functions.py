9010d32fe24d3a8041da0257a9937b03c915e661
KevinF42 <kforrister@segfault.com>
2017-10-30 09:05:37 -0700

    Add files via upload

---
+++ print_sfi_functions.py
@@ -0,0 +1,15 @@
+def intcalc():
+	A = int(5)
+	B = int(9)
+	print (A+B)
+def floatcalc():
+	C = float(12)
+	D = float(25)
+	print (C+D)
+def stringcat():
+	E = str("Abra")
+	F = str("Kadabra")
+	print (E+F)
+def main():
+	intcalc(),floatcalc(),stringcat()
+main()
