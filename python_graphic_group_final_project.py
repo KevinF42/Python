58c7d27ce2efcf331f461614ecd346c9b049da4f
KevinF42 <kforrister@segfault.com>
2017-10-30 09:03:32 -0700

    Add files via upload

---
+++ python_graphic_group_final_project.py
@@ -0,0 +1,810 @@
+import turtle   
+from turtle import *
+from random import randint 
+
+speed (0)
+                    
+bgcolor('black')
+      
+x = 1
+      
+while x < 400:
+      
+    r = randint(0,255) 
+    g = randint(0,255) 
+    b = randint(0,255) 
+
+    colormode(255) 
+               
+    
+    
+    pencolor(r,g,b) 
+                    
+      
+    fd(50 + x)
+    rt(90.911)
+    
+    
+    x = x+1 
+
+            # duty is heavier than a mountain, death lighter than a feather   
+reg = turtle.Turtle()      # create a turtle named alex
+mycolor=(255,255,255)
+reg.pencolor(mycolor)
+reg.penup()
+reg.backward(20)
+reg.pendown()
+reg.right(90)  
+reg.forward(13)           # 
+reg.left(90)               #
+reg.forward(1)
+reg.right(90)  
+reg.backward(4)
+reg.forward(22)
+reg.left(90)
+reg.forward(1)
+reg.right(90) 
+reg.backward(4)   
+#left cheekmark 39,139
+reg.left(90)
+reg.forward(4)
+reg.right(90)  
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)  
+reg.forward(14)
+#left cheekmark done 44,154
+reg.right(90)
+reg.forward(2)
+reg.right(90)  
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)  
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)  
+reg.backward(1)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)  
+reg.forward(11) #39,139
+reg.backward(11)
+reg.right(90)  
+reg.forward(2)
+reg.right(90)  
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)  
+reg.forward(2)
+reg.left(90)  
+reg.forward(1)
+reg.right(90)  
+reg.forward(2) #43,156
+reg.left(90) 
+reg.forward(1)
+reg.right(90)  
+reg.forward(1)
+reg.left(90) 
+reg.forward(1)
+reg.right(90)  
+reg.forward(1)
+reg.left(90) 
+reg.forward(1)
+reg.right(90)  
+reg.forward(1)
+reg.left(90) 
+reg.forward(1)
+reg.right(90)  
+reg.forward(1)
+reg.left(90)
+#red
+reg.forward(2)
+reg.left(90) 
+reg.forward(1)
+reg.right(90)  
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(2)
+reg.right(90)
+reg.forward(1)
+#red dot
+reg.left(90)
+reg.forward(3)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.backward(1)
+reg.forward(3)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(3)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(4)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(2)
+reg.right(90)
+reg.forward(1)
+reg.left(90) #68,169
+reg.forward(35)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(4)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(3)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(6)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(3) #122,164
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(3)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(4) # 137,150
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(2)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(5)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(5)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(3)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(4) #133,125
+reg.left(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(5)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(4)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2) #126,141
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(4)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(8)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(3)
+reg.left(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1) #137,151
+reg.right(90)
+reg.forward(2)
+reg.backward(1)
+reg.left(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(2)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.backward(1)
+reg.left(90)
+reg.forward(5) #153,142
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.backward(1)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(3)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1) #170,127
+#earstart
+reg.penup()
+reg.left(90)
+reg.forward(8)
+reg.pendown()
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(3)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(4)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(4)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(4)
+reg.right(90)
+reg.forward(2) #145,132
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(2)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(3)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(2)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(3)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(2)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+#earend
+#nosestart
+reg.penup() #170,128
+reg.backward(97)
+reg.right(90) #65 134
+reg.forward(6)
+reg.pendown()
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(2)
+reg.left(90)
+reg.forward(1)
+reg.right(90)
+reg.forward(1)
+reg.left(90)
+reg.forward(2) #66,139
+
+del turtle
+del randint
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("black")
+turtle = turtle.Turtle()
+turtle.speed(0)
+turtle.penup()
+turtle.shape("turtle")
+
+#This is the code that draws the initial rectangle that is the outline to the flag.
+def drawFillRectangle(x, y, length, width, color):
+    turtle.goto(x,y)
+    turtle.pendown()
+    turtle.color(color)
+    turtle.begin_fill()
+    turtle.forward(width)
+    turtle.right(90)
+    turtle.forward(length)
+    turtle.right(90)
+    turtle.forward(width)
+    turtle.right(90)
+    turtle.forward(length)
+    turtle.right(90)
+    turtle.end_fill()
+    turtle.penup()
+    
+#This is the code that draws the stars on the flag. It then repeats to properly fill the flag with stars.
+def drawStar(x,y,color,length) :
+    turtle.goto(x,y)
+    turtle.setheading(0)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.color(color)
+    for turn in range(0,5) :
+        turtle.forward(length)
+        turtle.right(144)
+        turtle.forward(length)
+        turtle.right(144)
+    turtle.end_fill()
+    turtle.penup()
+
+#This code draws out all the stripes on the flag.
+def drawStripes() :
+    x = -230
+    y = 125
+    for stripe in range(0,6) :
+        drawFillRectangle(x, y, 18, 460, 'red')
+        y = y - 20
+        drawFillRectangle(x, y, 18, 460, 'white')
+        y = y - 19
+    drawFillRectangle(x, y, 18, 460, 'red')
+    y = y - 20
+    
+#This code draws the quare where the stars go.
+def drawSquare() :
+    drawFillRectangle(-230, 125, 135, 150, 'navy')
+
+#Other code that actually inserts the stars.
+def drawSixStars() :
+    y = 115
+    for row in range(0,5) :
+        x = -230
+        y = y - 4
+        for star in range (0,6) :
+            x = x + 6
+            drawStar(x, y, 'white', 10)
+            x = x + 19
+        y = y - 20
+        
+#Other code that actually draws the stars
+def drawFiveStars() :
+    y = 103
+    for row in range(0,4) :
+        x = -212
+        y = y - 4
+        for star in range (0,5) :
+            drawStar(x, y, 'white', 10)
+            x = x + 25
+        y = y - 20
+
+drawStripes()
+drawSquare()
+drawSixStars()
+drawFiveStars()
+turtle.goto(0,-140)
+turtle.write("GOD BLESS THE USA")
+turtle.back(20)
+exitonclick()
+#import turtle
+
+#reg.reset()
+#reg.hideturtle()
+#reg.speed(0)
+
+
+#c = 0
+#x = 0
+
+#colors = [
+#reddish colors
+#(1.00, 0.00, 0.00),(1.00, 0.03, 0.00),(1.00, 0.05, 0.00),(1.00, 0.07, 0.00),(1.00, 0.10, 0.00),(1.00, 0.12, 0.00),(1.00, 0.15, 0.00),(1.00, 0.17, 0.00),(1.00, 0.20, 0.00),(1.00, 0.23, 0.00),(1.00, 0.25, 0.00),(1.00, 0.28, 0.00),(1.00, 0.30, 0.00),(1.00, 0.33, 0.00),(1.00, 0.35, 0.00),(1.00, 0.38, 0.00),(1.00, 0.40, 0.00),(1.00, 0.42, 0.00),(1.00, 0.45, 0.00),(1.00, 0.47, 0.00),
+#orangey colors
+#(1.00, 0.50, 0.00),(1.00, 0.53, 0.00),(1.00, 0.55, 0.00),(1.00, 0.57, 0.00),(1.00, 0.60, 0.00),(1.00, 0.62, 0.00),(1.00, 0.65, 0.00),(1.00, 0.68, 0.00),(1.00, 0.70, 0.00),(1.00, 0.72, 0.00),(1.00, 0.75, 0.00),(1.00, 0.78, 0.00),(1.00, 0.80, 0.00),(1.00, 0.82, 0.00),(1.00, 0.85, 0.00),(1.00, 0.88, 0.00),(1.00, 0.90, 0.00),(1.00, 0.93, 0.00),(1.00, 0.95, 0.00),(1.00, 0.97, 0.00),
+#yellowy colors
+#(1.00, 1.00, 0.00),(0.95, 1.00, 0.00),(0.90, 1.00, 0.00),(0.85, 1.00, 0.00),(0.80, 1.00, 0.00),(0.75, 1.00, 0.00),(0.70, 1.00, 0.00),(0.65, 1.00, 0.00),(0.60, 1.00, 0.00),(0.55, 1.00, 0.00),(0.50, 1.00, 0.00),(0.45, 1.00, 0.00),(0.40, 1.00, 0.00),(0.35, 1.00, 0.00),(0.30, 1.00, 0.00),(0.25, 1.00, 0.00),(0.20, 1.00, 0.00),(0.15, 1.00, 0.00),(0.10, 1.00, 0.00),(0.05, 1.00, 0.00),
+#greenish colors
+#(0.00, 1.00, 0.00),(0.00, 0.95, 0.05),(0.00, 0.90, 0.10),(0.00, 0.85, 0.15),(0.00, 0.80, 0.20),(0.00, 0.75, 0.25),(0.00, 0.70, 0.30),(0.00, 0.65, 0.35),(0.00, 0.60, 0.40),(0.00, 0.55, 0.45),(0.00, 0.50, 0.50),(0.00, 0.45, 0.55),(0.00, 0.40, 0.60),(0.00, 0.35, 0.65),(0.00, 0.30, 0.70),(0.00, 0.25, 0.75),(0.00, 0.20, 0.80),(0.00, 0.15, 0.85),(0.00, 0.10, 0.90),(0.00, 0.05, 0.95),
+#blueish colors
+#(0.00, 0.00, 1.00),(0.05, 0.00, 1.00),(0.10, 0.00, 1.00),(0.15, 0.00, 1.00),(0.20, 0.00, 1.00),(0.25, 0.00, 1.00),(0.30, 0.00, 1.00),(0.35, 0.00, 1.00),(0.40, 0.00, 1.00),(0.45, 0.00, 1.00),(0.50, 0.00, 1.00),(0.55, 0.00, 1.00),(0.60, 0.00, 1.00),(0.65, 0.00, 1.00),(0.70, 0.00, 1.00),(0.75, 0.00, 1.00),(0.80, 0.00, 1.00),(0.85, 0.00, 1.00),(0.90, 0.00, 1.00),(0.95, 0.00, 1.00)
+#]
+
+#while x < 1000:
+	#idx = int(c)
+	#color = colors[idx]
+	#reg.color(color)
+	#reg.forward(x)
+    #reg.right(98)
+    #x = x + 1
+    #c = c + 0.1
+
+#colors = [
+#reddish colors
+#(1.00, 0.00, 0.00),(1.00, 0.03, 0.00),(1.00, 0.05, 0.00),(1.00, 0.07, 0.00),(1.00, 0.10, 0.00),(1.00, 0.12, 0.00),(1.00, 0.15, 0.00),(1.00, 0.17, 0.00),(1.00, 0.20, 0.00),(1.00, 0.23, 0.00),(1.00, 0.25, 0.00),(1.00, 0.28, 0.00),(1.00, 0.30, 0.00),(1.00, 0.33, 0.00),(1.00, 0.35, 0.00),(1.00, 0.38, 0.00),(1.00, 0.40, 0.00),(1.00, 0.42, 0.00),(1.00, 0.45, 0.00),(1.00, 0.47, 0.00),
+#orangey colors
+#(1.00, 0.50, 0.00),(1.00, 0.53, 0.00),(1.00, 0.55, 0.00),(1.00, 0.57, 0.00),(1.00, 0.60, 0.00),(1.00, 0.62, 0.00),(1.00, 0.65, 0.00),(1.00, 0.68, 0.00),(1.00, 0.70, 0.00),(1.00, 0.72, 0.00),(1.00, 0.75, 0.00),(1.00, 0.78, 0.00),(1.00, 0.80, 0.00),(1.00, 0.82, 0.00),(1.00, 0.85, 0.00),(1.00, 0.88, 0.00),(1.00, 0.90, 0.00),(1.00, 0.93, 0.00),(1.00, 0.95, 0.00),(1.00, 0.97, 0.00),
+#yellowy colors
+#(1.00, 1.00, 0.00),(0.95, 1.00, 0.00),(0.90, 1.00, 0.00),(0.85, 1.00, 0.00),(0.80, 1.00, 0.00),(0.75, 1.00, 0.00),(0.70, 1.00, 0.00),(0.65, 1.00, 0.00),(0.60, 1.00, 0.00),(0.55, 1.00, 0.00),(0.50, 1.00, 0.00),(0.45, 1.00, 0.00),(0.40, 1.00, 0.00),(0.35, 1.00, 0.00),(0.30, 1.00, 0.00),(0.25, 1.00, 0.00),(0.20, 1.00, 0.00),(0.15, 1.00, 0.00),(0.10, 1.00, 0.00),(0.05, 1.00, 0.00),
+#greenish colors
+#(0.00, 1.00, 0.00),(0.00, 0.95, 0.05),(0.00, 0.90, 0.10),(0.00, 0.85, 0.15),(0.00, 0.80, 0.20),(0.00, 0.75, 0.25),(0.00, 0.70, 0.30),(0.00, 0.65, 0.35),(0.00, 0.60, 0.40),(0.00, 0.55, 0.45),(0.00, 0.50, 0.50),(0.00, 0.45, 0.55),(0.00, 0.40, 0.60),(0.00, 0.35, 0.65),(0.00, 0.30, 0.70),(0.00, 0.25, 0.75),(0.00, 0.20, 0.80),(0.00, 0.15, 0.85),(0.00, 0.10, 0.90),(0.00, 0.05, 0.95),
+#blueish colors
+#(0.00, 0.00, 1.00),(0.05, 0.00, 1.00),(0.10, 0.00, 1.00),(0.15, 0.00, 1.00),(0.20, 0.00, 1.00),(0.25, 0.00, 1.00),(0.30, 0.00, 1.00),(0.35, 0.00, 1.00),(0.40, 0.00, 1.00),(0.45, 0.00, 1.00),(0.50, 0.00, 1.00),(0.55, 0.00, 1.00),(0.60, 0.00, 1.00),(0.65, 0.00, 1.00),(0.70, 0.00, 1.00),(0.75, 0.00, 1.00),(0.80, 0.00, 1.00),(0.85, 0.00, 1.00),(0.90, 0.00, 1.00),(0.95, 0.00, 1.00)
+#]
+
+#while x < 1000:
+	#c = c + 0.1
+	#idx = int(c)
+	#color = colors[idx]
+	#turtle.color(color)
+	#turtle.forward(x)
+	#turtle.right(98)
+	#x = x + 1
+
+#c = 0
+#x = 0
+
+#hiscolors = [
+#reddish colors
+#(1.00, 0.00, 0.00),(1.00, 0.03, 0.00),(1.00, 0.05, 0.00),(1.00, 0.07, 0.00),(1.00, 0.10, 0.00),(1.00, 0.12, 0.00),(1.00, 0.15, 0.00),(1.00, 0.17, 0.00),(1.00, 0.20, 0.00),(1.00, 0.23, 0.00),(1.00, 0.25, 0.00),(1.00, 0.28, 0.00),(1.00, 0.30, 0.00),(1.00, 0.33, 0.00),(1.00, 0.35, 0.00),(1.00, 0.38, 0.00),(1.00, 0.40, 0.00),(1.00, 0.42, 0.00),(1.00, 0.45, 0.00),(1.00, 0.47, 0.00),
+#orangey colors
+#(1.00, 0.50, 0.00),(1.00, 0.53, 0.00),(1.00, 0.55, 0.00),(1.00, 0.57, 0.00),(1.00, 0.60, 0.00),(1.00, 0.62, 0.00),(1.00, 0.65, 0.00),(1.00, 0.68, 0.00),(1.00, 0.70, 0.00),(1.00, 0.72, 0.00),(1.00, 0.75, 0.00),(1.00, 0.78, 0.00),(1.00, 0.80, 0.00),(1.00, 0.82, 0.00),(1.00, 0.85, 0.00),(1.00, 0.88, 0.00),(1.00, 0.90, 0.00),(1.00, 0.93, 0.00),(1.00, 0.95, 0.00),(1.00, 0.97, 0.00),
+#yellowy colors
+#(1.00, 1.00, 0.00),(0.95, 1.00, 0.00),(0.90, 1.00, 0.00),(0.85, 1.00, 0.00),(0.80, 1.00, 0.00),(0.75, 1.00, 0.00),(0.70, 1.00, 0.00),(0.65, 1.00, 0.00),(0.60, 1.00, 0.00),(0.55, 1.00, 0.00),(0.50, 1.00, 0.00),(0.45, 1.00, 0.00),(0.40, 1.00, 0.00),(0.35, 1.00, 0.00),(0.30, 1.00, 0.00),(0.25, 1.00, 0.00),(0.20, 1.00, 0.00),(0.15, 1.00, 0.00),(0.10, 1.00, 0.00),(0.05, 1.00, 0.00),
+#greenish colors
+#(0.00, 1.00, 0.00),(0.00, 0.95, 0.05),(0.00, 0.90, 0.10),(0.00, 0.85, 0.15),(0.00, 0.80, 0.20),(0.00, 0.75, 0.25),(0.00, 0.70, 0.30),(0.00, 0.65, 0.35),(0.00, 0.60, 0.40),(0.00, 0.55, 0.45),(0.00, 0.50, 0.50),(0.00, 0.45, 0.55),(0.00, 0.40, 0.60),(0.00, 0.35, 0.65),(0.00, 0.30, 0.70),(0.00, 0.25, 0.75),(0.00, 0.20, 0.80),(0.00, 0.15, 0.85),(0.00, 0.10, 0.90),(0.00, 0.05, 0.95),
+#blueish colors
+#(0.00, 0.00, 1.00),(0.05, 0.00, 1.00),(0.10, 0.00, 1.00),(0.15, 0.00, 1.00),(0.20, 0.00, 1.00),(0.25, 0.00, 1.00),(0.30, 0.00, 1.00),(0.35, 0.00, 1.00),(0.40, 0.00, 1.00),(0.45, 0.00, 1.00),(0.50, 0.00, 1.00),(0.55, 0.00, 1.00),(0.60, 0.00, 1.00),(0.65, 0.00, 1.00),(0.70, 0.00, 1.00),(0.75, 0.00, 1.00),(0.80, 0.00, 1.00),(0.85, 0.00, 1.00),(0.90, 0.00, 1.00),(0.95, 0.00, 1.00)
+#]
+
+
+#while x < 8000:
+	#r = randint(0,255) 
+	#g = randint(0,255) 
+	#b = randint(0,255) 
+	#colormode(255)
+	#pencolor(r,g,b) 
+	#fd(x)
+	#rt(98)
+	#x = x + 1
+	#c = c + 0.1
+
+#This is the beginning to the code. This basically gets the Turtle going, importing the use of the program while the code runs.
+#This is the beginning to the code. This basically gets the Turtle going, importing the use of the program while the code runs.
+
+# What comes after the # is not seen by python. These are comments. :)
+
+from turtle import * # imports the module turtle,
+                    #* stands for all, which makes things easier
+
+speed(0) # sets the speed of drawing to 0, which is the fastest
+pencolor('purple') # sets the color of the pen/lines to white
+bgcolor('black') # sets the color of the background/canvas to black
+
+x = 0 # creates a variable x with value 0
+up() # lifts up the pen, so no lines are drawn
+
+#note fd() means move forward, bk() means move back
+# rt() or lt() means tilt right by a certain angle
+
+rt(45) 
+fd(90) 
+rt(135) 
+
+down() # sets down the pen, so that turtle can draw
+while x < 120: # while the value of x is lesser than 120,
+                #continuously do this:
+    fd(200)     
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+
+    rt(11.1111) 
+    x = x+1 # adds 1 to the value of x,
+            # so that it is closer to 120 after every loop
+
+exitonclick() # When you click, turtle exits.
+
+#That's all! Try customizing the script! 8)
+
+# What comes after the # is not seen by python. These are comments. :)
+
+from turtle import * # imports the module turtle,
+                    #* stands for all, which makes things easier
+
+speed(0) # sets the speed of drawing to 0, which is the fastest
+pencolor('red') # sets the color of the pen/lines to white
+bgcolor('black') # sets the color of the background/canvas to black
+
+x = 0 # creates a variable x with value 0
+up() # lifts up the pen, so no lines are drawn
+
+#note fd() means move forward, bk() means move back
+# rt() or lt() means tilt right by a certain angle
+
+rt(45) 
+fd(90) 
+rt(135) 
+
+down() # sets down the pen, so that turtle can draw
+while x < 120: # while the value of x is lesser than 120,
+                #continuously do this:
+    fd(200)     
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+
+    rt(11.1111) 
+    x = x+1 # adds 1 to the value of x,
+            # so that it is closer to 120 after every loop
+
+ # When you click, turtle exits.
+
+#That's all! Try customizing the script! 8)
+
+#This python code utilizies repeat turtle commands to properly draw out the flag and all of its components.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
